fadb51ebb7df35c429e6ca1889d7ed5519f8f02d
Alejandro Barrios <alejandro.barrios@acquia.com>
2020-04-15 21:34:52 -0400

    Moving files around

---
+++ bin/commands/internal/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# @TODO: Check if there are containers running, if so ask to destroy them, including volumes.
+
+# @TODO: Check if there is codebase in the 'html' directory. If so, ask to delete it.
+
+
+# Start Questionaire.
+
+echo "How many publishers?"
+read PUB
+echo "You selected $PUB publisher(s)."
