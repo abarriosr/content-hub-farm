e98720545ed7e0d303d9ad6ed3babaaaaab47217
Alejandro Barrios <alejandro.barrios@acquia.com>
2021-02-10 09:40:59 -0400

    Removing DrupalPractice from Coding Standards check

--- bin/commands/internal/phpcbf.sh
+++ bin/commands/internal/phpcbf.sh
@@ -3,4 +3,4 @@
 # Runs phpcs on the acquia_contenthub module.
 echo "Fixing Coding Standards for 'acquia_contenthub' module..."
 cd html
-./vendor/bin/phpcbf --standard=Drupal,DrupalPractice web/modules/contrib/acquia_contenthub/src web/modules/contrib/acquia_contenthub/tests
+./vendor/bin/phpcbf --standard=Drupal web/modules/contrib/acquia_contenthub/src web/modules/contrib/acquia_contenthub/tests
--- bin/commands/internal/phpcs.sh
+++ bin/commands/internal/phpcs.sh
@@ -3,4 +3,4 @@
 # Runs phpcs on the acquia_contenthub module.
 echo "Checking Coding Standards for 'acquia_contenthub' module..."
 cd html
-./vendor/bin/phpcs -n --standard=Drupal,DrupalPractice web/modules/contrib/acquia_contenthub/src web/modules/contrib/acquia_contenthub/tests
+./vendor/bin/phpcs -n --standard=Drupal web/modules/contrib/acquia_contenthub/src
