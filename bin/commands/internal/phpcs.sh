bc1373135ea6cb725b68521879db7e08dd3369ba
Alejandro Barrios <alejandro.barrios@acquia.com>
2021-05-24 17:40:07 -0400

    LCH-5000: Adding DOCROOT detection to commands

--- bin/commands/internal/phpcs.sh
+++ bin/commands/internal/phpcs.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Finding the DOCROOT...
+# @TODO: Find a better way to find out the docroot.
+if [ -d "html/docroot" ]
+then
+    echo "Using directory 'docroot' as the DOCROOT..."
+    DOCROOT='docroot';
+else
+    echo "Using directory 'web' as the DOCROOT..."
+    DOCROOT='web';
+fi
+
 # Runs phpcs on the acquia_contenthub module.
 echo "Checking Coding Standards for 'acquia_contenthub' module..."
 cd html
-./vendor/bin/phpcs -n --standard=Drupal web/modules/contrib/acquia_contenthub/src
+./vendor/bin/phpcs -n --standard=Drupal ${DOCROOT}/modules/contrib/acquia_contenthub/src
